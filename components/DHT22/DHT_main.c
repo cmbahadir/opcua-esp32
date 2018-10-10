dfe41ecda6402ca909ff31a5d86d5eae8d34c3a0
cmbahadir <c.mete.bahadir@gmail.com>
2018-10-10 18:52:41 +0300

    Added temperature sensor as component.

---
+++ components/DHT22/DHT_main.c
@@ -0,0 +1,48 @@
+/*
+
+	DHT22 sensor reading test
+
+	Jun 2007: Ricardo Timmermann, implemetation
+
+
+*/
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "rom/ets_sys.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+
+#include "DHT22.h"
+
+void DHT_task(void *pvParameter)
+{
+	setDHTgpio( 4 );
+	printf( "Starting DHT Task\n\n");
+
+	while(1) {
+	
+		printf("=== Reading DHT ===\n" );
+		int ret = readDHT();
+		
+		errorHandler(ret);
+
+		printf( "Hum %.1f\n", getHumidity() );
+		printf( "Tmp %.1f\n", getTemperature() );
+		
+		// -- wait at least 2 sec before reading again ------------
+		// The interval of whole process must be beyond 2 seconds !! 
+		vTaskDelay( 3000 / portTICK_RATE_MS );
+	}
+}
+
+void app_main()
+{
+	nvs_flash_init();
+	vTaskDelay( 1000 / portTICK_RATE_MS );
+	xTaskCreate( &DHT_task, "DHT_task", 2048, NULL, 5, NULL );
+}
+
