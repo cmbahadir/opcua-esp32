16fe898936bb89973152988e1f8b1c2ef5460fd2
cmbahadir <c.mete.bahadir@gmail.com>
2020-02-11 20:49:37 +0300

    OPC-UA and WIFI handlers are separated.

--- main/opcua_esp32.c
+++ main/opcua_esp32.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
+#include <sys/param.h>
+#include <unistd.h>
+#include <lwip/sockets.h>
+#include <esp_flash_encrypt.h>
+#include "esp_netif.h"
+#include <esp_task_wdt.h>
+#include <esp_sntp.h>
 #include "freertos/task.h"
 #include "driver/gpio.h"
-#include "sys/param.h"
 #include "sdkconfig.h"
-
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
 #include "esp_wifi.h"
@@ -12,67 +16,32 @@
 #include "esp_event.h"
 #include "nvs_flash.h"
 
-#include <time.h>
-#include <unistd.h>
-#include <lwip/sockets.h>
-#include <esp_flash_encrypt.h>
-#include "esp_netif.h"
 #include "open62541.h"
 #include "DHT22.h"
 #include "model.h"
-#include <esp_task_wdt.h>
-#include <esp_sntp.h>
 
 #define EXAMPLE_ESP_WIFI_SSID      CONFIG_WIFI_SSID
 #define EXAMPLE_ESP_WIFI_PASS      CONFIG_WIFI_PASSWORD
 #define EXAMPLE_ESP_MAXIMUM_RETRY  10
 
 #define TAG "OPCUA_ESP32"
+#define WIFI_TAG "WIFI"
+#define SNTP_TAG "STNP"
+#define MEMORY_TAG "MEMORY"
 #define ENABLE_MDNS
 
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT      BIT1
 
+static bool obtain_time(void);
+static void initialize_sntp(void);
+
 UA_ServerConfig *config;
 static UA_Boolean running = true;
 static EventGroupHandle_t s_wifi_event_group;
+static UA_Boolean isServerCreated = false;
 static int s_retry_num = 0;
-
-void time_sync_notification_cb(struct timeval *tv)
-{
-    ESP_LOGI(TAG, "Notification of a time synchronization event");
-}
-
-static void initialize_sntp(void)
-{
-    ESP_LOGI(TAG, "Initializing SNTP");
-    sntp_setoperatingmode(SNTP_OPMODE_POLL);
-    sntp_setservername(0, "pool.ntp.org");
-    sntp_set_time_sync_notification_cb(time_sync_notification_cb);
-    sntp_init();
-}
-
-static bool obtain_time(void)
-{
-    initialize_sntp();
-
-    ESP_ERROR_CHECK(esp_task_wdt_add(NULL));
-    // wait for time to be set
-    time_t now = 0;
-    struct tm timeinfo;
-    memset(&timeinfo, 0, sizeof(struct tm));
-    int retry = 0;
-    const int retry_count = 10;
-    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry <= retry_count) {
-        ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-        ESP_ERROR_CHECK(esp_task_wdt_reset());
-    }
-    time(&now);
-    localtime_r(&now, &timeinfo);
-    ESP_ERROR_CHECK(esp_task_wdt_delete(NULL));
-    return timeinfo.tm_year > (2016 - 1900);
-}
+RTC_DATA_ATTR static int boot_count = 0;
 
 static UA_StatusCode
 UA_ServerConfig_setUriName(UA_ServerConfig *uaServerConfig, const char *uri, const char *name) {
@@ -109,7 +78,6 @@ void opcua_task(void *pvParameter)
     ESP_LOGI(TAG, "Fire up OPC UA Server.");
     UA_Server *server = UA_Server_new();
     UA_ServerConfig *config = UA_Server_getConfig(server);
-    // UA_ServerConfig_setDefault(config);
     UA_ServerConfig_setMinimalCustomBuffer(config, 4840, 0, sendBufferSize, recvBufferSize);
 
     const char* appUri = "open62541.esp32.server";
@@ -134,7 +102,7 @@ void opcua_task(void *pvParameter)
         ESP_LOGI(TAG, "Could not get default IP Address!");
     }
     #endif
-    UA_ServerConfig_setUriName(config, appUri, "OPC UA Server - ESP32");
+    UA_ServerConfig_setUriName(config, appUri, "OPC_UA_Server_ESP32");
 
     /* Add Information Model Objects Here */
     addLEDMethod(server);
@@ -156,46 +124,98 @@ void opcua_task(void *pvParameter)
     ESP_ERROR_CHECK(esp_task_wdt_delete(NULL));
 }
 
-
-static void event_handler(void* arg, esp_event_base_t event_base,
-                                int32_t event_id, void* event_data)
+void time_sync_notification_cb(struct timeval *tv)
 {
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    ESP_LOGI(SNTP_TAG, "Notification of a time synchronization event");
+}
+
+static void initialize_sntp(void)
+{
+    ESP_LOGI(SNTP_TAG, "Initializing SNTP");
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, "pool.ntp.org");
+    // sntp_set_sync_mode(SNTP_SYNC_MODE_IMMED);
+    sntp_set_time_sync_notification_cb(time_sync_notification_cb);
+    sntp_init();
+}
+
+static bool obtain_time(void)
+{
+    initialize_sntp();
+
+    ESP_ERROR_CHECK(esp_task_wdt_add(NULL));
+    // wait for time to be set
+    time_t now = 0;
+    struct tm timeinfo;
+    memset(&timeinfo, 0, sizeof(struct tm));
+    int retry = 0;
+    const int retry_count = 10;
+    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry <= retry_count) {
+        ESP_LOGI(SNTP_TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        ESP_ERROR_CHECK(esp_task_wdt_reset());
+    }
+    time(&now);
+    localtime_r(&now, &timeinfo);
+    ESP_ERROR_CHECK(esp_task_wdt_delete(NULL));
+    return timeinfo.tm_year > (2016 - 1900);
+}
+
+//Event Handler for WIFI events - it triggered when ESP_EVENT_ANY_ID event occurs.
+static void wifi_event_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
         esp_wifi_connect();
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY)
+        {
             esp_wifi_connect();
             s_retry_num++;
-            ESP_LOGI(TAG, "retry to connect to the AP");
-        } else {
+            ESP_LOGI(WIFI_TAG, "retry to connect to the AP");
+        }
+        else
+        {
             xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
         }
-        ESP_LOGI(TAG,"connect to the AP fail");
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        ESP_LOGI(WIFI_TAG, "connect to the AP fail");
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        ESP_LOGI(WIFI_TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
         s_retry_num = 0;
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
 
-        time_t now;
-        struct tm timeinfo;
-        time(&now);
-        localtime_r(&now, &timeinfo);
-        // Is time set? If not, tm_year will be (1970 - 1900).
-        if (timeinfo.tm_year < (2016 - 1900)) {
-            ESP_LOGI(TAG, "Time is not set yet. Connecting to WiFi and getting time over NTP.");
-            if (!obtain_time()) {
-                ESP_LOGE(TAG, "Could not get time from NTP. Using default timestamp.");
-            }
-            // update 'now' variable with current time
-            time(&now);
+//Event handler for OPC Server - it is triggered when IP_EVENT_STA_GOT_IP occurs
+static void opc_event_handler(void *arg, esp_event_base_t event_base,
+                              int32_t event_id, void *event_data)
+{
+    time_t now;
+    struct tm timeinfo;
+    time(&now);
+    localtime_r(&now, &timeinfo);
+    if (timeinfo.tm_year < (2016 - 1900))
+    {
+        ESP_LOGI(SNTP_TAG, "Time is not set yet. Connecting to WiFi and getting time over NTP.");
+        if (!obtain_time())
+        {
+            ESP_LOGE(SNTP_TAG, "Could not get time from NTP. Using default timestamp.");
         }
-        localtime_r(&now, &timeinfo);
-        ESP_LOGI(TAG, "Current time: %d-%02d-%02d %02d:%02d:%02d", timeinfo.tm_year + 1900, timeinfo.tm_mon+1, timeinfo.tm_mday, timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
-
-
-        xTaskCreatePinnedToCore(&opcua_task, "opcua_task", 24336, NULL, 1, NULL, 1);
-        ESP_LOGI(TAG, "Heap size after OPC UA Task : %d", esp_get_free_heap_size());
+        time(&now);
+    }
+    localtime_r(&now, &timeinfo);
+    ESP_LOGI(SNTP_TAG, "Current time: %d-%02d-%02d %02d:%02d:%02d", timeinfo.tm_year + 1900, timeinfo.tm_mon + 1, timeinfo.tm_mday, timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+    if (!isServerCreated)
+    {
+        xTaskCreatePinnedToCore(&opcua_task, "opcua_task", 24336, NULL, 10, NULL, 0);
+        ESP_LOGI(MEMORY_TAG, "Heap size after OPC UA Task : %d", esp_get_free_heap_size());
+        isServerCreated = true;
     }
 }
 
@@ -213,8 +233,12 @@ static void wifi_scan(void)
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_task_wdt_init(10, true));
+    ESP_ERROR_CHECK(esp_task_wdt_delete(xTaskGetIdleTaskHandleForCPU(0)));
+    ESP_ERROR_CHECK(esp_task_wdt_delete(xTaskGetIdleTaskHandleForCPU(1)));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &opc_event_handler, NULL));
 
     wifi_config_t wifi_config = {
         .sta = {
@@ -233,13 +257,13 @@ static void wifi_scan(void)
                                            portMAX_DELAY);
     if (bits & WIFI_CONNECTED_BIT)
     {
-        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
-                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+        ESP_LOGI(WIFI_TAG, "connected to ap SSID:%s",
+                 EXAMPLE_ESP_WIFI_SSID);
     }
     else if (bits & WIFI_FAIL_BIT)
     {
-        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
-                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s",
+                 EXAMPLE_ESP_WIFI_SSID);
     }
     else
     {
@@ -249,15 +273,17 @@ static void wifi_scan(void)
     ESP_ERROR_CHECK(tcpip_adapter_set_hostname(TCPIP_ADAPTER_IF_STA, "ESP32_OPCUA_Server"));
     ESP_ERROR_CHECK(tcpip_adapter_dhcpc_start(TCPIP_ADAPTER_IF_STA));
 
-    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler));
-    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &opc_event_handler));
     vEventGroupDelete(s_wifi_event_group);
 }
 
 void app_main()
 {
+    ++boot_count;
     //Encryption improvement w.r.t: 
     //Espressif security advisory concerning fault injection and secure boot (CVE-2019-15894)
+    spi_flash_init();
     if (esp_flash_encryption_enabled()) 
     {
         esp_flash_write_protect_crypt_cnt();
@@ -270,7 +296,4 @@ void app_main()
         ESP_ERROR_CHECK(nvs_flash_init());
     }
     wifi_scan();
-    ESP_ERROR_CHECK(esp_task_wdt_init(10, true));
-    ESP_ERROR_CHECK(esp_task_wdt_delete(xTaskGetIdleTaskHandleForCPU(0)));
-    ESP_ERROR_CHECK(esp_task_wdt_delete(xTaskGetIdleTaskHandleForCPU(1)));
 }
