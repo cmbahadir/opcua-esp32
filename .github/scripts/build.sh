47878eb8689eb5d453302c4fb49372a58c9b10e3
cmbahadir <c.mete.bahadir@gmail.com>
2023-01-22 18:24:28 +0300

    Migrate to v5.0 of ESP-IDF

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd $HOME
-git clone --recursive https://github.com/espressif/esp-idf.git
+git clone -b v5.0 --recursive https://github.com/espressif/esp-idf.git
 cd esp-idf
 bash install.sh
 source export.sh
